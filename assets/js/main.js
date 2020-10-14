bbceafcd9d8f9c09270d550be2d44a4646007d11
= <=>
2020-10-14 15:04:36 +0700

    change image size low, homePage (index)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,32 @@
 $('.parallax-window').parallax({
     imageSrc: './assets/img/jGuCm.png',
-    speed: 0.1,
+    speed: 0.3,
+});
+
+$('.friends-data').parallax({
+    imageSrc: './assets/img/friends-data.png',
+    speed: 0.9,
+});
+
+$('.friend-3').parallax({
+    imageSrc: './assets/img/bustle/2020/2020-4.jpg',
+    speed: 0.6,
+});
+
+$('.file-data').parallax({
+    imageSrc: './assets/img/file-data.png',
+    speed: 0.9,
+});
+
+
+$('.friend-200x').parallax({
+    imageSrc: './assets/img/2013-1.png',
+    speed: 0.9,
+});
+
+$('.friend-c3').parallax({
+    imageSrc: './assets/img/2015-1.jpg',
+    speed: 0.95,
 });
 
 $('.parallax-window-2').parallax({
